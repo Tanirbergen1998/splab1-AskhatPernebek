0cf087856ba44f4a22b6760550395d2d3651b06d
AskhatPernebek <32272697+AskhatPernebek@users.noreply.github.com>
2017-09-25 22:31:11 +0600

    Add files via upload

--- sincos.c
+++ sincos.c
@@ -1,9 +1,27 @@
-//Fullname: Vasya Pupkin
-//Group: 3EN04X
+//Fullname: Pernebek Askhat
+//Group: 3EN04B
+
 
 #include <stdio.h>
 #include <math.h>
 
 int main(int argc, char*argv[]){
-  return 0;
+  double a;
+	if(argc > 2){
+		sscanf(argv[2], "%lf", &a);
+		if(!strcmp(argv[1], "-s")){
+			printf("%f\n",sin(a));
+		}else if(!strcmp(argv[1], "-c")){
+			printf("%f\n",cos(a));
+		}
+		
+	}else if(argc == 2){
+		sscanf(argv[1], "%lf", &a);
+		printf("%f\n",sin(a));
+		printf("%f\n",cos(a));
+	}else{
+		printf("usage: sincos angle\nonly sine: sincos -s angle\nonly cosine: sincos -c angle\n");
+	}
+	return 0;
+
 }
